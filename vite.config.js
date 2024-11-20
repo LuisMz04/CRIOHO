256eef648866a6d443ef773428958e38f8dde24c
LuisMz04 <157434146+LuisMz04@users.noreply.github.com>
2024-11-19 21:25:40 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,42 +5,36 @@ import htmlPurge from 'vite-plugin-purgecss';
 import path, { resolve } from "node:path";
 import handlebars from 'vite-plugin-handlebars';
 
-export default defineConfig({
-    appType: "mpa",
-    base: "/CRIOHO/", 
-    build: {
-        rollupOptions: {
-            input: Object.fromEntries(
-                [
-                    ...glob.sync('./!(dist)/*.html').map(
-                        file => [
-                            file.slice(0, file.length - path.extname(file).length),
-                            resolve(__dirname, file)
-                        ]
-                    ),
-                    ...glob.sync('./*.html').map(
-                        file => [
-                            file.slice(0, file.length - path.extname(file).length),
-                            resolve(__dirname, file)
-                        ]
-                    )
-                ]
-            )
-        }
-    },
-    css: {
-    
-        preprocessorOptions: {
-            less: {
-                javascriptEnabled: true,
-            },
+export default defineConfig(
+    {
+        appType: "mpa",
+        base: "/CRIOHO/",
+        build: {
+            rollupOptions: {
+                input: Object.fromEntries(
+                    [
+                        ...glob.sync('./!(dist)/*.html').map(
+                            file=> [
+                                file.slice(0, file.length - path.extname(file).length),
+                                resolve(__dirname, file)
+                            ]
+                        ),
+                        ...glob.sync('./*.html').map(
+                            file=> [
+                                file.slice(0, file.length - path.extname(file).length),
+                                resolve(__dirname, file)
+                            ]
+                        )
+                    ]
+                )
+            }
         },
-    },
-    plugins: [
-        handlebars({
-            partialDirectory: resolve(__dirname, 'partials'),
-        }),
-        htmlPurge({}),
-        ViteMinifyPlugin(),
-    ],
-});
+        plugins: [
+            handlebars({
+                partialDirectory: resolve(__dirname, 'partials'),
+    }),
+            htmlPurge({}),
+            ViteMinifyPlugin()
+        ]
+    }
+);
